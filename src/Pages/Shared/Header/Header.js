c5a03ea2ae016f868a3d5d983916367b72bcd9ef
sajib <me.mrsajib@gmail.com>
2022-04-26 20:48:17 +0600

    conditional nav item show and signOut user

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,8 +1,16 @@
+import { signOut } from "firebase/auth";
 import React from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
 import { Link } from "react-router-dom";
+import auth from "../../../firebase.init";
 import "./Header.css";
 
 const Header = () => {
+  const [user] = useAuthState(auth);
+
+  const logout = () => {
+    signOut(auth);
+  };
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="container">
@@ -24,22 +32,50 @@ const Header = () => {
           <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
             <li className="nav-item">
               <Link to="/" className="nav-link active">
-                Home
+                HOME
               </Link>
             </li>
             <li className="nav-item">
               <Link to="/blog" className="nav-link ">
-                Blog
+                BLOG
               </Link>
             </li>
-            <li className="nav-item">
-              <Link
-                to="/registration"
-                className="nav-link btn btn-primary text-white"
-              >
-                Registration
-              </Link>
-            </li>
+            {user ? (
+              <div className="d-flex">
+                <li className="nav-item">
+                  <Link to="/event" className="nav-link">
+                    EVENTS
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link to="/add" className="nav-link">
+                    ADD VOLUNTEER
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link to="/admin" className="nav-link">
+                    ADMIN
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <button
+                    onClick={() => logout()}
+                    className="nav-link btn-link btn"
+                  >
+                    Log-out
+                  </button>
+                </li>
+              </div>
+            ) : (
+              <li className="nav-item">
+                <Link
+                  to="/registration"
+                  className="nav-link btn btn-primary text-white"
+                >
+                  Registration
+                </Link>
+              </li>
+            )}
           </ul>
         </div>
       </div>
