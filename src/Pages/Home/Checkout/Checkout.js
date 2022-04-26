cdf590070480062bbd866f24c50f26fd49d7dfdd
sajib <me.mrsajib@gmail.com>
2022-04-26 21:38:10 +0600

    create form and send to doner information

--- src/Pages/Home/Checkout/Checkout.js
+++ src/Pages/Home/Checkout/Checkout.js
@@ -1,7 +1,10 @@
 import React, { useEffect, useState } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
 import { useParams } from "react-router-dom";
+import auth from "../../../firebase.init";
 
 const Checkout = () => {
+  const [user] = useAuthState(auth);
   const { donationId } = useParams();
   const [donation, setDonation] = useState([]);
   useEffect(() => {
@@ -9,6 +12,24 @@ const Checkout = () => {
       .then((res) => res.json())
       .then((data) => setDonation(data[0]));
   }, [donationId]);
+
+  const addVolunteer = (e) => {
+    e.preventDefault();
+    const volunteerName = donation.name;
+    const name = e.target.name.value;
+    const email = user.email;
+    const date = e.target.date.value;
+    const image = donation.img;
+    const volunteerId = donation._id;
+    const data = { volunteerName, volunteerId, name, email, image, date };
+    fetch("http://localhost:5000/volunteer", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data),
+    })
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  };
   return (
     <div className="container">
       <div className="my-5">
@@ -33,7 +54,45 @@ const Checkout = () => {
               Contact: <b>{donation.phone}</b>
             </p>
             <p>website: {donation?.website}</p>
-            <button className="btn btn-primary">DONATE NOW</button>
+
+            <form onSubmit={addVolunteer}>
+              <div className="form-group mb-3">
+                <label htmlFor="name">Name</label>
+                <input
+                  name="name"
+                  value={user?.displayName}
+                  type="text"
+                  className="form-control"
+                  id="name"
+                  readOnly
+                />
+              </div>
+              <div className="form-group mb-3">
+                <label htmlFor="email">Email address</label>
+                <input
+                  name="email"
+                  value={user?.email}
+                  type="email"
+                  className="form-control"
+                  id="email"
+                  readOnly
+                />
+              </div>
+              <div className="form-group mb-3">
+                <label htmlFor="date">Visit Date</label>
+                <input
+                  name="date"
+                  type="date"
+                  required
+                  className="form-control"
+                  id="date"
+                />
+              </div>
+
+              <button type="submit" className="btn btn-primary">
+                DONATE NOW
+              </button>
+            </form>
           </div>
         </div>
       </div>
