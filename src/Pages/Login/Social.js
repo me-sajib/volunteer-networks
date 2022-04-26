3e2e600e35e8ddb5a9665b5ddaea06c9ed80010d
sajib <me.mrsajib@gmail.com>
2022-04-26 18:50:05 +0600

    social login pages add

---
+++ src/Pages/Login/Social.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Social = () => {
+  return (
+    <div className="container">
+      <div className="w-50">
+        <button className="btn btn-primary-outline">Google</button>
+      </div>
+    </div>
+  );
+};
+
+export default Social;
