c794045fc570ef4700f50ad495aadfa5948b0847
sajib <me.mrsajib@gmail.com>
2022-04-26 21:01:21 +0600

    navigate to previous page and loading spinner add

--- src/Pages/Login/Social.js
+++ src/Pages/Login/Social.js
@@ -1,21 +1,29 @@
 import React from "react";
 import { useSignInWithGoogle } from "react-firebase-hooks/auth";
+import { useLocation, useNavigate } from "react-router-dom";
 import auth from "../../firebase.init";
+import Spinner from "../Shared/Spinner/Spinner";
 
 const Social = () => {
   const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+  const location = useLocation();
+  const navigate = useNavigate();
+  let from = location.state?.from?.pathname || "/";
+
   if (user) {
-    console.log(user);
+    navigate(from, { replace: true });
   }
   return (
     <div className="container">
       <div className="mt-5">
+        {loading && <Spinner />}
         <button
           onClick={() => signInWithGoogle()}
           className="btn btn-outline-primary shadow rounded d-block w-100 "
         >
           Google
         </button>
+        {error && <div className="alert alert-danger">{error.message}</div>}
       </div>
     </div>
   );
