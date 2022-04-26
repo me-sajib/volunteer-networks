b7633a9449fe5ea6a76785ea39ac379557c86bb0
sajib <me.mrsajib@gmail.com>
2022-04-26 20:47:42 +0600

    login with google auth provider

--- src/Pages/Login/Social.js
+++ src/Pages/Login/Social.js
@@ -1,10 +1,19 @@
 import React from "react";
+import { useSignInWithGoogle } from "react-firebase-hooks/auth";
+import auth from "../../firebase.init";
 
 const Social = () => {
+  const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+  if (user) {
+    console.log(user);
+  }
   return (
     <div className="container">
       <div className="mt-5">
-        <button className="btn btn-outline-primary shadow rounded d-block w-100 ">
+        <button
+          onClick={() => signInWithGoogle()}
+          className="btn btn-outline-primary shadow rounded d-block w-100 "
+        >
           Google
         </button>
       </div>
