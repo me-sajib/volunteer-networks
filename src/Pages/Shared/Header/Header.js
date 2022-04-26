a23a3ec3df244905b7f11da7cc3a5276f41cbb18
sajib <me.mrsajib@gmail.com>
2022-04-26 17:48:10 +0600

    navbar switch to right side

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -20,10 +20,24 @@ const Header = () => {
           <a class="navbar-brand" href="/">
             Hidden brand
           </a>
-          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
             <li class="nav-item">
               <a class="nav-link active" aria-current="page" href="/">
                 Home
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link " aria-current="page" href="/">
+                Blog
+              </a>
+            </li>
+            <li class="nav-item">
+              <a
+                class="nav-link btn btn-primary text-white"
+                aria-current="page"
+                href="/"
+              >
+                Registration
               </a>
             </li>
           </ul>
