1b251d9e00f1ebb4fbf4673ec543fd1415bfcc86
sajib <me.mrsajib@gmail.com>
2022-04-27 10:50:23 +0600

    doner event show page create

---
+++ src/Pages/Dashboard/Event.js
@@ -0,0 +1,42 @@
+import React from "react";
+import "./css/event.css";
+
+const Event = ({ donate }) => {
+  const { image, volunteerName, date, _id } = donate;
+
+  const removeEvent = (id) => {
+    const sure = window.confirm("Are you sure you want to remove this event?");
+    fetch("http://localhost:5000/donate/" + id, {
+      method: "DELETE",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (sure) {
+          window.location.reload();
+        }
+      });
+  };
+  return (
+    <div className="col shadow rounded eventWidth mx-3">
+      <div className="d-flex gap-3">
+        <div className="img">
+          <img src={image} alt="" />
+        </div>
+        <div className="details">
+          <h2>{volunteerName}</h2>
+          <p>{date}</p>
+          <div className="cancelBtn ">
+            <button
+              onClick={() => removeEvent(_id)}
+              className="btn btn-outline-dark "
+            >
+              Cancel
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Event;
