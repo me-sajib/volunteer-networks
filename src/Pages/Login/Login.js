378440439bd0c924da2f56071ceb2747c3b5869b
sajib <me.mrsajib@gmail.com>
2022-04-27 21:20:19 +0600

    user login with email password done

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,16 +1,32 @@
 import React from "react";
+import { useSignInWithEmailAndPassword } from "react-firebase-hooks/auth";
+import auth from "../../firebase.init";
+import Spinner from "../Shared/Spinner/Spinner";
 import Social from "./Social";
 
 const Login = () => {
+  const [signInWithEmailAndPassword, user, loading, error] =
+    useSignInWithEmailAndPassword(auth);
+
+  const loginUser = async (e) => {
+    e.preventDefault();
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    await signInWithEmailAndPassword(email, password);
+    if (user) {
+      window.location = "/";
+    }
+  };
   return (
     <div className="container">
       <div className="w-50 mx-auto mt-4">
         <h2 className="text-primary text-center">Login now</h2>
-        <form>
+        <form onSubmit={loginUser}>
           <div className="form-group mb-4">
             <label htmlFor="exampleInputEmail1">Email address</label>
             <input
               type="email"
+              name="email"
               className="form-control"
               id="exampleInputEmail1"
               aria-describedby="emailHelp"
@@ -21,12 +37,18 @@ const Login = () => {
             <label htmlFor="exampleInputPassword1">Password</label>
             <input
               type="password"
+              name="password"
               className="form-control"
               id="exampleInputPassword1"
               placeholder="Password"
             />
           </div>
-          <button className="btn btn-primary">Login</button>
+          {error && <div className="alert alert-danger">{error.message}</div>}
+          <button type="submit" className="btn btn-primary">
+            Login
+          </button>
+
+          {loading && <Spinner />}
         </form>
         <Social />
       </div>
