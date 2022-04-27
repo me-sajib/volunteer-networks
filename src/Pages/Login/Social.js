5dfb9b2d4ca1abf6f13026396a1e89b5d427c398
sajib <me.mrsajib@gmail.com>
2022-04-27 20:19:58 +0600

    add to fontawesome

--- src/Pages/Login/Social.js
+++ src/Pages/Login/Social.js
@@ -16,12 +16,32 @@ const Social = () => {
   return (
     <div className="container">
       <div className="mt-5">
+        <div className="d-flex mb-3">
+          <div class="bg-primary w-100 mt-2" style={{ height: "2px" }}></div>
+          <div className="mx-2">OR</div>
+          <div
+            className="bg-primary w-100 mt-2"
+            style={{ height: "2px" }}
+          ></div>
+        </div>
         {loading && <Spinner />}
         <button
           onClick={() => signInWithGoogle()}
-          className="btn btn-outline-primary shadow rounded d-block w-100 "
+          className="btn btn-outline-primary shadow rounded mx-3 w-25 "
         >
-          Google
+          <i class="fa-brands fa-google"></i>
+        </button>
+        <button
+          onClick={() => signInWithGoogle()}
+          className="btn btn-outline-primary shadow rounded  w-25 "
+        >
+          <i class="fa-brands fa-github"></i>
+        </button>
+        <button
+          onClick={() => signInWithGoogle()}
+          className="btn btn-outline-primary shadow rounded ms-3  w-25 "
+        >
+          <i class="fa-brands fa-facebook"></i>
         </button>
         {error && <div className="alert alert-danger">{error.message}</div>}
       </div>
