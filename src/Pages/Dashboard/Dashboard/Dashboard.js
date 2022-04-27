3ab2a7e345232e8cbbbf6b9a20a038dfaf942b0d
sajib <me.mrsajib@gmail.com>
2022-04-27 11:20:53 +0600

    all donar show in dashboard and delete

---
+++ src/Pages/Dashboard/Dashboard/Dashboard.js
@@ -0,0 +1,68 @@
+import React, { useEffect, useState } from "react";
+import Spinner from "../../Shared/Spinner/Spinner";
+
+const Dashboard = () => {
+  const [donar, setDonar] = useState([]);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    fetch("http://localhost:5000/donate")
+      .then((res) => res.json())
+      .then((data) => setDonar(data));
+    setLoading(false);
+  }, []);
+  let total = 0;
+  //   remove donar
+  const removeDonar = (id) => {
+    const sure = window.confirm("Are you sure you want to remove this event?");
+    fetch("http://localhost:5000/donar/" + id, {
+      method: "DELETE",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (sure) {
+          window.location.reload();
+        }
+      });
+  };
+  return (
+    <div className="container">
+      <h1>Dashboard</h1>
+      <table className="table table-striped table-primary">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">NAME</th>
+            <th scope="col">EMAIL</th>
+            <th scope="col">VOLUNTEER</th>
+            <th scope="col">DATE</th>
+            <th scope="col">ACTION</th>
+          </tr>
+        </thead>
+        <tbody>
+          {donar.map((donar) => {
+            return (
+              <tr key={donar._id}>
+                <th scope="row">{++total}</th>
+                <td>{donar.name}</td>
+                <td>{donar.email}</td>
+                <td>{donar.volunteerName}</td>
+                <td>{donar.date}</td>
+                <td>
+                  <button
+                    className="btn btn-danger"
+                    onClick={() => removeDonar(donar._id)}
+                  >
+                    DELETE
+                  </button>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+      {loading && <Spinner />}
+    </div>
+  );
+};
+
+export default Dashboard;
